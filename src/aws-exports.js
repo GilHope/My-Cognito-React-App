a6f08b002cc8dc9067dd4012f24fa37cb8813bba
GilHope <ghopecs@gmail.com>
2023-09-15 22:30:18 -0400

    updates to app.js and create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,7 @@
+export const awsExports = {
+    "REGION": "us-east-1",
+    "USER_POOL_ID": "us-east-1_EMGUrPHxo",
+    "USER_POOL_CLIENT_ID": "5g4gjg64ktggqg5cagdoiqemfa"
+}
+
+// Parameters required for application to login and registration with Cognito UserPool.
