1bd01050e118b7dad8eaa45b8228d51975b06bcd
GilHope <ghopecs@gmail.com>
2023-09-15 23:34:22 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -10,15 +10,87 @@ Amplify.configure({
     userPoolId: awsExports.USER_POOL_ID,
     userPoolWebClientId: awsExports.USER_POOL_CLIENT_ID
   }
-})
+});
 
 function App() {
+  const [jwtToken, setJwtToken] = useState('');
+
+  useEffect(() => {
+    fetchJwtToken();
+  }, []);
+  
+  const fetchJwtToken = async () => {
+    try {
+      const session = await Auth.currentSession();
+      const token = session.getIdToken().getJwtToken();
+      setJwtToken(token);
+    } catch (error) {
+      console.log('Error fetching JWT token:', error);
+    }
+  };
+  
+  
   return (
-    <Authenticator>
-      {({ signOut, user }) =>(
-        <div>
-          <p>Welcome {user.username}</p>
-          <button onClick={signOut}>Sign out</button>
+    <Authenticator initialState='signIn'
+    components={{
+      SignUp: {
+        FormFields() {
+
+          return (
+            <>
+              <Authenticator.SignUp.FormFields />
+
+              {/* Custom fields for given_name and family_name */}
+              <div><label>First name</label></div>
+              <input
+                type="text"
+                name="given_name"
+                placeholder="Please enter your first name"
+              />
+              <div><label>Last name</label></div>
+              <input
+                type="text"
+                name="family_name"
+                placeholder="Please enter your last name"
+              />
+              <div><label>Email</label></div>
+              <input
+                type="text"
+                name="email"
+                placeholder="Please enter a valid email"
+              />
+
+
+            </>
+          );
+        },
+      },
+    }}
+    services={{
+      async validateCustomSignUp(formData) {
+        if (!formData.given_name) {
+          return {
+            given_name: 'First Name is required',
+          };
+        }
+        if (!formData.family_name) {
+          return {
+            family_name: 'Last Name is required',
+          };
+        }
+        if (!formData.email) {
+          return {
+            email: 'Email is required',
+          };
+        }
+      },
+    }}
+    >
+      {({ signOut, user}) => (
+        <div>Welcome {user.username}
+        <button onClick={signOut}>Sign out</button>
+        <h4>Your JWT token:</h4>
+        {jwtToken}
         </div>
       )}
     </Authenticator>
